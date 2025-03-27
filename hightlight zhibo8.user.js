2c18b9c879d68894af47fb74618ad301330c1e7c
yinxiao <rooneyv8@163.com>
2025-03-27 10:07:31 +0800

    improve the zhibo8 news page

--- hightlight zhibo8.user.js
+++ hightlight zhibo8.user.js
@@ -2,11 +2,14 @@
 // @name        hightlight zhibo8
 // @match       *://*.zhibo8.com/*
 // @author      yinxiao
-// @version      0.2.5
+// @version      0.2.6
 // @description 把点击过的直播吧新闻链接颜色改为醒目色
 // @updateURL https://github.com/rubinbaby/userscripts/blob/main/hightlight%20zhibo8.user.js
 // @downloadURL https://github.com/rubinbaby/userscripts/blob/main/hightlight%20zhibo8.user.js
+// @grant        GM.xmlHttpRequest
+// @connect      www.zhibo8.com
 // ==/UserScript==
+
 function containsChinese(str, substring) {
     const regex = new RegExp(substring, 'u');
     return regex.test(str);
@@ -16,6 +19,131 @@ function stopAutoRefresh() {
     console.log("start to disable auto refrensh");
     window.stop();
     console.log("end to disable auto refrensh");
+}
+
+function changeLinkColor(visitedLinks){
+    $("a").each(function (index, element) {
+        visitedLinks.forEach(function (link) {
+            if (containsChinese(element.href, link)) {
+                $(element).attr("style", "color:#BC62C2");
+            }
+        })
+    });
+}
+
+function getPastDays(num) {
+  const dates = [];
+  const today = new Date();
+
+  for (let i = 0; i < num; i++) {
+    const pastDate = new Date(today);
+    pastDate.setDate(today.getDate() - i);  // 逐日回退[2](@ref)
+    dates.push(pastDate.toISOString().split('T')[0]);  // 格式化为YYYY-MM-DD
+  }
+
+  return dates.reverse(); // 按日期升序排列
+}
+
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
+}
+
+function promiseRequest(config) {
+  return new Promise((resolve, reject) => {
+    GM.xmlHttpRequest({
+      ...config,
+      onload: (res) => {
+        if (res.status >= 200 && res.status < 300) {
+          resolve(res.responseText);
+        } else {
+          reject(new Error(`HTTP ${res.status}`));
+        }
+      },
+      onerror: reject,
+      ontimeout: reject
+    });
+  });
+}
+
+function listAllNews(visitedLinks){
+    let requests = [];
+    const _dates = getPastDays(14);
+    _dates.forEach(function(_date){
+        requests.push(promiseRequest({url:'https://news.zhibo8.com/zuqiu/json/'+_date+'.htm'}));
+        requests.push(promiseRequest({url:'https://www.zhibo8.com/zuqiu/json/'+_date+'.htm'}));
+    });
+
+    Promise.all(requests)
+    .then((results) => {
+        //console.log('聚合数据:', results);
+        var _news = []
+        results.forEach(function(data){
+            //console.log(data);
+            var dataJson = JSON.parse(data);
+            var news = [];
+            if("video_arr" in dataJson){
+                news = [...news, ...dataJson.video_arr];
+            }
+            if("video" in dataJson){
+                news = [...news, ...dataJson.video];
+            }
+            news.forEach(function(_new){
+                var label = _new.lable;
+                if(containsChinese(label, '曼联')){
+                    if(_new.type === 'zuqiu'){
+                        _new.type2 = '新闻';
+                    }else if(_new.type === 'zuqiujijin'){
+                        _new.type2 = '集锦';
+                    }else if(_new.type==='zuqiuluxiang'){
+                        _new.type2 = '录像';
+                    }
+                    _news.push(_new);
+                }
+            });
+        });
+
+        _news.sort((a,b)=>{
+            var a_split = a.createtime.split(" ");
+            var b_split = b.createtime.split(" ");
+            var dateDiff = new Date(b_split[0]) - new Date(a_split[0]);
+            if (dateDiff !== 0) return dateDiff;
+
+            // 日期相同则按时间排序
+            return b_split[1].localeCompare(a_split[1]);
+        });
+        //console.log(_news);
+        var footerDiv = document.getElementById('footer');
+        var newDiv = document.createElement('div');
+        newDiv.id = 'body';
+        newDiv.style.margin = '20px';
+        footerDiv.parentNode.insertBefore(newDiv, footerDiv);
+
+        const ul = document.createElement('ul');
+        ul.style.listStyle = 'none';
+        _news.forEach(function(_new){
+            const li = document.createElement('li');
+            // 创建 a 元素并设置属性
+            const a = document.createElement('a');
+            a.href = (_new.type=="zuqiu"?"//news.zhibo8.com":"//www.zhibo8.com")+_new.url;
+            a.textContent = "["+_new.type2+"] "+_new.title+" ["+_new.createtime+"]";
+            a.target = '_blank';
+//             a.style.color = '#007BFF';  // 可选：设置链接颜色
+//             a.style.textDecoration = 'underline';  // 可选：添加下划线
+
+            // 将 a 元素添加到 li 中
+            li.appendChild(a);
+            ul.appendChild(li);
+        });
+        newDiv.appendChild(ul);
+        changeLinkColor(visitedLinks);
+    })
+    .catch(err => console.log('部分请求失败', err));
 }
 
 (function () {
@@ -29,13 +157,7 @@ function stopAutoRefresh() {
             visitedLinks.push(v.replace("-news-zuqiu-", "/").replaceAll("_", "-"));
         })
     });
-    $("a").each(function (index, element) {
-        visitedLinks.forEach(function (link) {
-            if (containsChinese(element.href, link)) {
-                $(element).attr("style", "color:#BC62C2");
-            }
-        })
-    });
+    changeLinkColor(visitedLinks);
     var hostname = window.location.hostname;
     var zoom = '1';
     if (containsChinese(hostname, 'zhibo8.com')) {
@@ -45,6 +167,7 @@ function stopAutoRefresh() {
     var pathname = window.location.pathname;
     if (containsChinese(pathname, '/zuqiu/more.htm')) {
         zoom = '1.5';
+        listAllNews(visitedLinks);
         setTimeout(stopAutoRefresh, 20000);
     }
     if (containsChinese(pathname, '/zuqiu/')
