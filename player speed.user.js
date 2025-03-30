5f433c7f7386ee0f2dd545ec3c5c5e75cbf8132f
yinxiao <rooneyv8@163.com>
2025-03-30 23:08:11 +0800

    add local video player speed

--- player speed.user.js
+++ player speed.user.js
@@ -3,49 +3,50 @@
 // @description This is your new file, start writing code
 // @match     https://www.youtube.com/*
 // @match     https://www.bilibili.com/*
+// @match     file:///Users/*
 // @author yinxiao
-// @version      0.1.1
+// @version      0.1.2
 // @updateURL https://github.com/rubinbaby/userscripts/blob/main/player%20speed.user.js
 // @downloadURL https://github.com/rubinbaby/userscripts/blob/main/player%20speed.user.js
 // ==/UserScript==
 function containsChinese(str, substring) {
-  const regex = new RegExp(substring, 'u');
-  return regex.test(str);
+    const regex = new RegExp(substring, 'u');
+    return regex.test(str);
 }
 
-function changeVideoPlaySpeed(speed){
-    var v=document.getElementsByTagName('video');
-    for (var i=0;i<v.length;i++){
+function changeVideoPlaySpeed(speed) {
+    var v = document.getElementsByTagName('video');
+    for (var i = 0; i < v.length; i++) {
         var original_speed = v[i].playbackRate;
-        if(original_speed!=speed){
-            console.log("original play speed: ", original_speed, ", current play speed: ",speed);
+        if (original_speed != speed) {
+            console.log("original play speed: ", original_speed, ", current play speed: ", speed);
             v[i].playbackRate = speed;
         }
     }
 }
 
-function changeVideoPlayMode(domain){
-    if(containsChinese(domain, "www.youtube.com")){
+function changeVideoPlayMode(domain) {
+    if (containsChinese(domain, "www.youtube.com")) {
         var doms = document.getElementsByClassName("ytp-size-button ytp-button");
-        if(doms.length > 0){
-            for(var i=0;i<doms.length;i++){
-                if(containsChinese(doms[i].title,"Cinema mode")){
+        if (doms.length > 0) {
+            for (var i = 0; i < doms.length; i++) {
+                if (containsChinese(doms[i].title, "Cinema mode")) {
                     doms[i].click();
                 }
             }
         }
         var chatCloseDoms = document.querySelectorAll("#close-button > yt-button-renderer button");
-        if(chatCloseDoms.length > 0){
-            for(var j=0;j<chatCloseDoms.length;j++){
+        if (chatCloseDoms.length > 0) {
+            for (var j = 0; j < chatCloseDoms.length; j++) {
                 chatCloseDoms[j].click();
             }
         }
     }
-    if(containsChinese(domain, "www.bilibili.com")){
+    if (containsChinese(domain, "www.bilibili.com")) {
         var doms = document.getElementsByClassName("bpx-player-ctrl-btn bpx-player-ctrl-wide");
-        if(doms.length > 0){
-            for(var i=0;i<doms.length;i++){
-                if(doms[i].classList.length<3){
+        if (doms.length > 0) {
+            for (var i = 0; i < doms.length; i++) {
+                if (doms[i].classList.length < 3) {
                     doms[i].click();
                 }
             }
@@ -53,30 +54,35 @@ function changeVideoPlayMode(domain){
     }
 }
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
     var domain = window.location.hostname;
+    var href = window.location.href;
     var title = document.title;
     var speed = 1;
-    if(containsChinese(domain, "www.youtube.com")){
+    if (containsChinese(domain, "www.youtube.com")) {
         var keyword = "list=PLh9lJwqeOuvNPqHfKf10o5Ql9M-OEnoLy";
-//         keyword = "kpjOEw8a9K0";
+        // keyword = "kpjOEw8a9K0";
         var isUrlMatched = window.location.href.includes(keyword);
-//         console.log("url is matched: "+isUrlMatched);
-        if(isUrlMatched){
+        // console.log("url is matched: " + isUrlMatched);
+        if (isUrlMatched) {
             speed = 1.5;
         }
     }
-    if(containsChinese(domain, "www.bilibili.com")){
-        if(containsChinese(title,"名侦探柯南")){
+    if (containsChinese(domain, "www.bilibili.com")) {
+        if (containsChinese(title, "名侦探柯南")) {
             speed = 1.25;
         }
     }
 
-    window.setInterval(function(){
+    if (containsChinese(href, "file:///Users/")) {
+        speed = 1.5;
+    }
+
+    window.setInterval(function () {
         changeVideoPlaySpeed(speed);
         changeVideoPlayMode(domain);
-    },5000);
+    }, 5000);
 })();
