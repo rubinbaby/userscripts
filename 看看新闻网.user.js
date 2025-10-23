0e9ac6ca65c85e66212d28a665d5a3108e083c68
yinxiao <rooneyv8@163.com>
2025-10-23 21:13:00 +0800

    去除header方便iframe显示

--- 看看新闻网.user.js
+++ 看看新闻网.user.js
@@ -2,7 +2,7 @@
 // @name        看看新闻网
 // @match       *://*.kankanews.com/k24
 // @author      yinxiao
-// @version      0.0.1
+// @version      0.0.2
 // @description 看看新闻网24小时进行时页面更新
 // @updateURL https://github.com/rubinbaby/userscripts/blob/main/hightlight%20kankanews.user.js
 // @downloadURL https://github.com/rubinbaby/userscripts/blob/main/hightlight%20kankanews.user.js
@@ -16,6 +16,10 @@
     document.body.style.zoom = zoom;
     var k24Ele = document.querySelector('.k24');
     if (k24Ele) {
-        k24Ele.setAttribute("style", "margin:unset");
+        k24Ele.setAttribute("style", "margin:unset;padding:unset");
     }
+
+    document.querySelector('header').remove();
+    document.querySelector('.k24 .k24-content').setAttribute('style', 'grid-column:1/8');
+    document.querySelector('.k24 .k24-aside').setAttribute('style', 'grid-column:8/11');
 })();
