e6ef8cc5e3eebc705d302ea266b58d5bf283e509
yinxiao <rooneyv8@163.com>
2025-10-24 12:26:04 +0800

    隐藏部分元素以适合iframe显示

--- 比分直播.user.js
+++ 比分直播.user.js
@@ -4,7 +4,7 @@
 // @match       https://www.188bifen.com/
 // @match       http://www.188bifen.com/
 // @author yinxiao
-// @version      0.2.4
+// @version      0.2.5
 // @updateURL https://github.com/rubinbaby/userscripts/blob/main/%E6%AF%94%E5%88%86%E7%9B%B4%E6%92%AD.user.js
 // @downloadURL https://github.com/rubinbaby/userscripts/blob/main/%E6%AF%94%E5%88%86%E7%9B%B4%E6%92%AD.user.js
 // ==/UserScript==
@@ -26,6 +26,8 @@ function stopScoreAlert() {
     'use strict';
 
     // Your code here...
+    document.querySelector('.vct-menu').setAttribute('style', 'display:none');
+    document.querySelector('.vct-container .top-act').setAttribute('style', 'display:none');
     var original_session = window.sessionStorage.selectLeagueIds;
     var new_session = '{"zuqiu":{"21":1,"22":1,"24":1,"371":1,"369":1,"395":1,"372":1,"383":1,"7":1,"352":1,"353":1}}';
     console.log("original session: ", original_session);
